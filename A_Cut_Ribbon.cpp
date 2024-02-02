9ad0f724fb3c27c5922bbb94bb0a5dfb45228a0f
Pranto Bala <128365505+Pranto210102@users.noreply.github.com>
2024-02-02 22:51:08 +0600

    Add files via upload

---
+++ A_Cut_Ribbon.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,a,b,c;
+    cin>>n>>a>>b>>c;
+   int  dp[n+1];
+    dp[0]=0;
+    for(int i=1;i<=n;i++){
+        int x=INT_MIN;
+        int y=INT_MIN;
+        int z=INT_MIN;
+        if(i>=a)
+        {
+            x=dp[i-a];
+        }
+        if(i>=b)
+        { 
+            y=dp[i-b];
+        }
+        if(i>=c)
+        {
+            z=dp[i-c];
+        }
+        dp[i]= 1+ max(x, max(y,z));
+    }
+    cout<<dp[n]<<endl;
+}
