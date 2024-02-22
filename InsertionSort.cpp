b889e8add948256274a187673319716f1b344f4d
Pranto Bala <128365505+Pranto210102@users.noreply.github.com>
2024-02-22 13:41:48 +0600

    Update InsertionSort.cpp

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,26 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
-void Sort_fnc(int a[], int n)
+void print(int a[], int n)
 {
-    for(int i=1;i<n;i++){
+    for(int i=0;i<n;i++)
+    cout<<a[i]<<" ";
+    cout<<endl;
+}
+void to_sort(int a[], int n)
+{
+     print(a, n); //before sort
+
+    for(int i=1;i<n;i++)
+    {
         int key=a[i];
         int j=i-1;
-        while(j>0 && a[j]>key)
+        while(j>=0 and a[j]>key) //Decrement loop
         {
-            a[j+1]=a[j];
-            j-=1;
-            a[j+1]=key;
+            a[j+1]=a[j]; //shipt to add the key val
+            j--;
         }
+        a[j+1]=key; // fill the blank cell
+        print(a, n);
     }
-
-    for(int i=0;i<n;i++)
-    {
-        cout<<a[i]<<" ";
-    }
-    cout<<endl;
+    print(a, n);
 }
 int main()
 {
-    int a[10]={1,6,3,7,4,3,9,8,8,2};
-    Sort_fnc(a, 10);
+    int a[]{12,6,9,7,3,11,43,21};
+    int n=sizeof(a)/sizeof(a[0]);
+    to_sort(a, n);
 }
